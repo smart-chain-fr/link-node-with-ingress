9c347605ea47aa1eec9e90bdfba696d5b133866a
gisele-igore <gisele.dekpe@smart-chain.fr>
2023-05-22 12:10:32 +0200

    Cleaning the directories

--- charts/tezos/scripts/upgrade-storage.sh
+++ charts/tezos/scripts/upgrade-storage.sh
@@ -5,4 +5,5 @@ then
   printf "No store in data dir found, probably initial start, doing nothing."
   exit 0
 fi
+rm -rf /var/tezos/node/data/*
 octez-node upgrade storage --config /etc/tezos/config.json
